fc8e832125a541d19868df56ec43d62b3a26fd62
Abel Ayalew Abebe <103965027+abela2112@users.noreply.github.com>
2024-05-03 03:09:47 +0300

    update index.html

--- script.js
+++ script.js
@@ -1,21 +1,50 @@
 const button = document.querySelector('.menu');
+const homelink = document.querySelector(".home");
+const aboutlink = document.querySelector(".about");
+const contactlink = document.querySelector(".contact");
+const projectlink = document.querySelector(".project");
+const skillsLink = document.querySelector(".skills");
 
-button.addEventListener('click', () => {
-    document.querySelector('.side-bar').classList.toggle('hidden');
-    // document.querySelector('.side-bar').classList.toggle('hidden');
-
+homelink.addEventListener("click", () => {
+  document.querySelector(".side-bar").classList.toggle("hidden");
+  // document.querySelector('.side-bar').classList.toggle('hidden');
 });
-const menuButton = document.querySelector('.menu-button');
-const navbarLinks = document.querySelector('.navbar-links');
+aboutlink.addEventListener("click", () => {
+  document.querySelector(".side-bar").classList.toggle("hidden");
+  // document.querySelector('.side-bar').classList.toggle('hidden');
+});
+projectlink.addEventListener("click", () => {
+  document.querySelector(".side-bar").classList.toggle("hidden");
+  // document.querySelector('.side-bar').classList.toggle('hidden');
+});
+skillsLink.addEventListener("click", () => {
+  document.querySelector(".side-bar").classList.toggle("hidden");
+  // document.querySelector('.side-bar').classList.toggle('hidden');
+});
+projectlink.addEventListener("click", () => {
+  document.querySelector(".side-bar").classList.toggle("hidden");
+  // document.querySelector('.side-bar').classList.toggle('hidden');
+});
+contactlink.addEventListener("click", () => {
+  document.querySelector(".side-bar").classList.toggle("hidden");
+  // document.querySelector('.side-bar').classList.toggle('hidden');
+});
+button.addEventListener("click", () => {
+  document.querySelector(".side-bar").classList.toggle("hidden");
+  // document.querySelector('.side-bar').classList.toggle('hidden');
+});
+const menuButton = document.querySelector(".menu-button");
+const navbarLinks = document.querySelector(".navbar-links");
 
-document.addEventListener('DOMContentLoaded', (event) => {
-    // Your code here
-    const closeBtn = document.querySelector(".close-btn");
-    if(closeBtn) { // check if closeBtn is not null
-        closeBtn.addEventListener('click', () => {
-            document.querySelector('.side-bar').classList.add('hidden');
-        });
-    }
+document.addEventListener("DOMContentLoaded", (event) => {
+  // Your code here
+  const closeBtn = document.querySelector(".close-btn");
+  if (closeBtn) {
+    // check if closeBtn is not null
+    closeBtn.addEventListener("click", () => {
+      document.querySelector(".side-bar").classList.add("hidden");
+    });
+  }
 });
 
 // menuButton.addEventListener('click', () => {
@@ -28,17 +57,15 @@ document.addEventListener('DOMContentLoaded', (event) => {
 //     console.log('closing')
 //    // document.querySelector('.side-bar').remove();
 // });
-const observer=new IntersectionObserver(entries=>{
-    entries.forEach(entry=>{
-       
-        const img=entry.target
-     
-        if(entry.isIntersecting){
-            console.log('value',entry.isIntersecting) 
-            img.classList.add('img-bg-animation')
-            return
-        }
-        img.classList.remove('img-bg-animation')
-    })
-})
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    const img = entry.target;
+    if (entry.isIntersecting) {
+      console.log("value", entry.isIntersecting);
+      img.classList.add("img-bg-animation");
+      return;
+    }
+    img.classList.remove("img-bg-animation");
+  });
+});
 observer.observe(document.querySelector('.img-bg'))
